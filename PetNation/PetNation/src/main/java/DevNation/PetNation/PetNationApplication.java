6c6ff0ee86f2c223c31e36f06b18dbf5a5f1a34b
Gustavo Rutiquewiski <gustavo.rutiquewiski@outlook.com>
2022-08-15 16:12:01 -0300

    Teste 2

--- PetNation/PetNation/src/main/java/DevNation/PetNation/PetNationApplication.java
+++ PetNation/PetNation/src/main/java/DevNation/PetNation/PetNationApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PetNationApplication {
-
-	//API Start
 	public static void main(String[] args) {
 		SpringApplication.run(PetNationApplication.class, args);
 	}
